c182d94301843f4840519f9b314e0b9668c9897d
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-08-26 09:48:48 -0300

    remove `button`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,11 @@ import { Suspense } from 'react';
 import styles from './page.module.css';
 import List from '@/components/List';
 
-//TODO: IMPLEMENTAR FEATURE DE LOGOUT
-
 export default function Home() {
 	return (
 		<div className={styles.page}>
 			<header className={styles.header}>
 				<h1 className={styles.title}>Teste Chat2Desk</h1>
-				<button className={styles.logout}>Logout</button>
 			</header>
 			<main className={styles.main}>
 				<Suspense fallback={<p className={styles.load}>Loading...</p>}>
